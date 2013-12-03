4ea3a042a43f90c0720fea3bfbf2ea2d7d1d8125
Brandon Toner <brandontoner@gmail.com>
2013-12-03 09:01:48 -0800

    Tue Dec  3 09:01:48 PST 2013

--- main.cpp
+++ main.cpp
@@ -27,70 +27,82 @@ double rand_exp(double lambda) {
     double p = rand_double();
     return -log(1 - p) / lambda;
 }
-//       25 items         1 customer             20 items
-//  ------------------- x ---------- x 4 lanes = --------
-//  1 customer * 1 lane   5 minutes              1 minute
+
+double average(double values[], int n) {
+    double average = 0;
+    for (int i = 0; i < n; ++i) {
+        average += values[i] / n;
+    }
+    return average;
+}
+
+double standard_deviation(double values[], int n, double avg) {
+    double stddev = 0;
+    for (int i = 0; i < n; ++i) {
+        double diff = values[i] - avg;
+        stddev += diff * diff / n;
+    }
+    return sqrt(stddev);
+}
+
+double standard_deviation(double values[], int n) {
+    double avg = average(values, n);
+    return standard_deviation(values, n, avg);
+}
+
+//  25 items      0.1 minutes     4.15 customers    10 lanes
+// ---------- x ---------------- x ------------- x
+// 1 customer   1 item  * 1 lane     1 minute
 class customer {
 private:
-    const static double mean_time_per_item  = 0.05;
+    const static double mean_time_per_item  = 0.1;
     const static double stdev_time_per_item = 0.01;
     const static double mean_item_count     = 25;
-    const static double stdev_item_count    = 5;
-public:
-    const int    items_count;
-    const double time;
-          double time_left;
+    const static double stdev_item_count    = 10;
 
-    customer(int items, double t) : items_count(items), time(t), time_left(t) {
+    double my_time_left;
+public:
+    const int items_count;
+
+    customer(int items, double t) : items_count(items), my_time_left(t) { }
+
+    void decrease_time_left(double t) {
+        my_time_left -= t;
+    }
+
+    double time_left() const {
+        return my_time_left;
     }
 
     static customer random_customer() {
         int items;
-        double time = 0;
         do {
             items = (int) rand_normal(mean_item_count, stdev_item_count);
         } while(items <= 0);
 
-	for(int i = 0; i < items; ++i) {
-            time += rand_normal(mean_time_per_item, stdev_time_per_item);
-	}
+        // Distribution of a sum of normally distributed values is normal with
+        // mean n * mu and standard deviation sigma * sqrt(n)
+        double time = rand_normal(mean_time_per_item * items,
+                                  stdev_time_per_item * sqrt(items));
+
+	// for(int i = 0; i < items; ++i) {
+        //     time += rand_normal(mean_time_per_item, stdev_time_per_item);
+	// }
 	return customer(items, time);
     }
 };
 
 class lane {
   private:
+    static double total_wait_time;
+    static int    customers_processed;
+
     queue<customer> customers;
   public:
-    const static int    express_limit = 15;
-          static double total_wait_time;
-          static int    customers_processed;
-
-    const bool is_express;
+    const static int  express_limit = 25;
+    const        bool is_express;
 
     lane(bool express) : is_express(express) {
-    }
-
-    void process(const double time) {
-        if (customers.empty()) return;
-        double front_time_left = customers.front().time_left;
-        if (front_time_left < time) {
-            // First customer isn't using all the time
-            // All customers had to wait for the first customer to be processed
-            total_wait_time += customers.size() * front_time_left;
-            // First customer is removed from the line
-            customers.pop();
-            // Front customer was processed
-            customers_processed++;
-            // process the rest of the time
-            process(time - front_time_left);
-	} else {
-            // First customer is using all the time
-            // All customers had to wait for 'time'
-            total_wait_time             += customers.size() * time;
-            // Decrease the front customers time left
-            customers.front().time_left -= time;
-        }
     }
 
     int size() const {
@@ -105,23 +117,55 @@ class lane {
         customers.push(customer);
     }
 
+    const customer& front() const {
+        return customers.front();
+    }
+
+    bool can_enter(const customer& c) const {
+        return is_express == false || c.items_count <= express_limit;
+    }
+
+    void process(const double time) {
+        if (customers.empty()) return;
+        double front_time_left = customers.front().time_left();
+        if (front_time_left <= time) {
+            // First customer isn't using all the time
+            // All customers had to wait for the first customer to be processed
+            total_wait_time += customers.size() * front_time_left;
+            // First customer is removed from the line
+            customers.pop();
+            // Front customer was processed
+            customers_processed++;
+            // Recursively process the rest of the time
+            process(time - front_time_left);
+	} else {
+            // First customer is using all the time
+            // All customers had to wait for 'time'
+            total_wait_time += customers.size() * time;
+            // Decrease the front customers time left
+            customers.front().decrease_time_left(time);
+        }
+    }
+
     static void reset() {
         total_wait_time = 0;
         customers_processed = 0;
+    }
 
+    static double average_wait_time() {
+        if (customers_processed == 0) throw 10;
+        return total_wait_time / customers_processed;
     }
 };
 
 int    lane::customers_processed = 0;
 double lane::total_wait_time     = 0;
 
-lane* get_best_line(lane* lanes[], int lanes_count, int items) {
+lane* get_best_line(lane* lanes[], int lanes_count, const customer& c) {
     lane* best_line = NULL;
-    int best_size = 10E6;
-    for (int i = 1; i < lanes_count; ++i) {
-        if (lanes[i]->size() < best_size &&
-           (lanes[i]->is_express == false ||
-            lanes[i]->is_express && items < lane::express_limit)) {
+    int best_size = 2E9;
+    for (int i = 0; i < lanes_count; ++i) {
+        if (lanes[i]->size() < best_size && lanes[i]->can_enter(c)) {
             best_line = lanes[i];
             best_size = lanes[i]->size();
         }
@@ -130,46 +174,54 @@ lane* get_best_line(lane* lanes[], int lanes_count, int items) {
 }
 
 int main() {
-    const double mean_time_between_customers = 1;
-    const int    lanes_count                 = 5;
-    const int    express_lanes_count         = 1;
-    const double max_time                    = 8 * 60;
+    const double mean_customers_per_minute = 4.15;
+    const int    lanes_count               = 10;
+    const int    express_lanes_count       = 2;
+    const double max_time                  = 8 * 60; // 8 hours
+    const int    iteration_count           = 100000;
+          double times[iteration_count];
 
     // Seed the random number generator;
     srand(time(NULL));
 
-    double k = 0;
 
-    for(int i = 0; i < 10000; ++i) {
+    for(int i = 0; i < iteration_count; ++i) {
+        // Reset customer count and total waiting time to zero
         lane::reset();
-        lane* lines[lanes_count];
+        // Create lanes
+        lane* lanes[lanes_count];
         for(int j = 0; j < lanes_count; ++j) {
-            lines[j] = new lane(j < express_lanes_count);
+            lanes[j] = new lane(j < express_lanes_count);
         }
 
-        double time = 0;
-        while (time < max_time) {
+        for(double time = 0; time < max_time;) {
+            // New customer appears after a wait of "new_time"
+            double   new_time     = rand_exp(mean_customers_per_minute);
             customer new_customer = customer::random_customer();
-            double   new_time     = rand_exp(1 / mean_time_between_customers);
-            for(int i = 0; i < lanes_count; ++i) {
-                lines[i]->process(new_time);
-            }
-            lane* best_line = get_best_line(lines, lanes_count, new_customer.items_count);
 
-            // put customer in line
-            best_line->add(new_customer);
+            // All the lanes have been processing customers for "new_time"
+            for(int j = 0; j < lanes_count; ++j) {
+                lanes[j]->process(new_time);
+            }
+            // Get the best lane for the customer to enter
+            lane* best_lane = get_best_line(lanes, lanes_count, new_customer);
+            // Put customer in line
+            best_lane->add(new_customer);
+            // Increase the time
             time += new_time;
         }
 
-        // printf("%-50s %d\n", "Customers processed:", line::customers_processed);
-        // printf("%-50s %f\n", "Total wait time:",     line::total_wait_time);
-        // printf("%-50s %f\n", "Mean wait time:",      line::total_wait_time / line::customers_processed);
-        // printf("%f\n", lane::total_wait_time / lane::customers_processed);
-        k += lane::total_wait_time / lane::customers_processed;
-        printf("%f\n", k / (i + 1));
-        for(int i = 0; i < lanes_count; ++i) {
-            delete lines[i];
+        times[i] = lane::average_wait_time();
+        printf("%f\n", times[i]);
+
+        // Free resources
+        for(int j = 0; j < lanes_count; ++j) {
+            delete lanes[j];
         }
     }
+
+    double avg = average(times, iteration_count);
+    double stddev  = standard_deviation(times, iteration_count, avg);
+    printf("avg %0.4f std-dev %0.4f\n", avg, stddev);
 }
 
